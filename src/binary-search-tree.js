132eb887ea9218ee6199e39a4ba26504691c1ef2
Max Komarov <b-raven@yandex.ru>
2022-05-02 19:32:39 +0300

    feat: add binary tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -26,13 +26,28 @@ class BinarySearchTree {
 
   add(data) {
     let newNode = new Node(data);
-    if (this.root === null) {
-        this.root = newNode;
+    if (this.Root === null) {
+        this.Root = newNode;
     } else {
-        this.insertNode(this.root, newNode);
+        this.insertNode(this.Root, newNode);
     }
     
   }
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+        if (node.left === null) {
+            node.left = newNode;
+        } else {
+            this.insertNode(node.left, newNode);
+        }
+    } else {
+        if (node.right === null) {
+            node.right = newNode;
+        } else {
+            this.insertNode(node.right, newNode);
+        }
+    }
+}
 
   has(/* data */) {
     throw new NotImplementedError('Not implemented');
